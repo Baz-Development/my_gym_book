5f787aea3b010a17615f6f6a8b1928b936bb3fc9
Felipe Baz <fbazmitsuishi@gmail.com>
2023-05-25 17:51:18 -0300

    add workouts in group

--- lib/common/models/group_model.dart
+++ lib/common/models/group_model.dart
@@ -4,23 +4,27 @@ class GroupModel {
   String groupId;
   String name;
   List<UserModel> users;
+  List<String> workouts;
 
   GroupModel(
       this.groupId,
       this.name,
-      this.users,
-      );
+      this.users, {
+        this.workouts = const [],
+      });
 
   GroupModel.fromJson(Map<String, dynamic> json)
       : groupId = json['groupId'],
         name = json['name'],
         users = (json['users'] as List<dynamic>)
             .map((userJson) => UserModel.fromJson(userJson))
-            .toList();
+            .toList(),
+        workouts = (json['workouts'] as List<dynamic>).cast<String>();
 
   Map<String, dynamic> toJson() => {
     'groupId': groupId,
     'name': name,
     'users': users.map((user) => user.toJson()).toList(),
+    'workouts': workouts,
   };
 }
